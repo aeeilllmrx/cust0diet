56d47cb43042b9f33c92b6c6dab89865b1aa4639
alex <alexmiller.ml@gmail.com>
2020-09-27 17:33:29 -0400

    add host

--- server.py
+++ server.py
@@ -4,4 +4,4 @@ from flask import Flask
 
 # create flask app that does nothing
 app = Flask(__name__)
-app.run(os.environ.get('PORT'))
+app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT'))
